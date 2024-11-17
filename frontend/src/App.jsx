034681983bcb680d6d9875742adc62a932d33316
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-11-17 23:04:33 +0500

    signup page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,9 +11,9 @@ import Sidebar from "./components/Sidebar";
 function App() {
 
   return (
-    <div className="flex text-white">
+    <div className="flex">
       <Sidebar />
-      <div className="max-w-5xl my-5 mx-auto transition-all duration-300 flex-1">
+      <div className="max-w-5xl text-white mx-auto transition-all duration-300 flex-1">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
@@ -21,7 +21,6 @@ function App() {
           <Route path="/explore" element={<ExplorePage />} />
           <Route path="/likes" element={<LikesPage />} />
         </Routes>
-        <footer>Footer</footer>
       </div>
     </div>
   )
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,11 +7,10 @@ import { MdEditDocument } from "react-icons/md";
 import Logout from "./Logout";
 
 const Sidebar = () => {
-    const authUser = true;
+    const authUser = false;
     return (
         <aside
-            className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8
-      overflow-y-auto border-r bg-glass'
+            className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-l-0 border-t-0 border-b-0 border-r bg-glass'
         >
             <nav className='h-full flex flex-col gap-3'>
                 <Link to='/' className='flex justify-center'>
