8900206e42a14fb60fe82b36b28fb89750032cce
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-11-23 23:27:15 +0500

    user model + database implementation

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import userRoutes from './routes/user.route.js';
 import exploreRoutes from './routes/explore.route.js';
+import { connectMongoDB } from './db/connect.js';
 
 dotenv.config();
 
@@ -20,5 +21,6 @@ app.use('/api/users', userRoutes)
 app.use('/api/explore', exploreRoutes)
 
 app.listen(5000, () => {
+    connectMongoDB();
     console.log('Server is running on port http://localhost:5000');
 })
