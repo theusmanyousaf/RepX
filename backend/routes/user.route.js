9912803b715c760c6a1ce9b8d7eeea4d9997ef6e
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-11-24 13:48:39 +0500

    user profile like functionality and ui

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { getUserProfileAndRepos } from "../controllers/user.controller.js";
+import { getLikes, getUserProfileAndRepos, likeProfile } from "../controllers/user.controller.js";
+import { ensureAuthenticated } from "../middleware/authenticated.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", getUserProfileAndRepos)
 // TODO: add like route where users and like a profile
+router.post("/like/:username", ensureAuthenticated, likeProfile)
 // TODO: add likes route to get all users that like a profile
+router.get("/likes", ensureAuthenticated, getLikes)
 
 export default router;
--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -4,23 +4,9 @@ import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
 import { formatMemberSince } from "../utils/functions";
+import LikeProfile from "./LikeProfile";
 
 const ProfileInfo = ({ userProfile }) => {
-    // const userProfile = {
-    // 	avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-    // 	bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-    // 	email: "johndoe@gmail.com",
-    // 	followers: 100,
-    // 	following: 200,
-    // 	html_url: "https://github.com/burakorkmez",
-    // 	location: "Somewhere, Earth",
-    // 	name: "John Doe",
-    // 	public_gists: 100,
-    // 	public_repos: 100,
-    // 	twitter_username: "johndoe",
-    // 	login: "johndoe",
-    // };
-
     const memberSince = formatMemberSince(userProfile?.created_at);
 
     return (
@@ -34,6 +20,7 @@ const ProfileInfo = ({ userProfile }) => {
                     {/* View on Github */}
 
                     <div className='flex gap-2 items-center flex-col'>
+                        <LikeProfile userProfile={userProfile} />
                         <a
                             href={userProfile?.html_url}
                             target='_blank'
