bdb2ee0f83cb7189d880e5a08046515ae3ac4398
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-11-24 13:02:26 +0500

    login and logout funcitonality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage";
@@ -11,19 +11,19 @@ import Sidebar from "./components/Sidebar";
 import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  const { authUser } = useAuthContext();
-  console.log("authenticated user: ", authUser);
+  const { authUser, loading } = useAuthContext();
 
+  if (loading) return;
   return (
-    <div className="flex">
+    <div className='flex'>
       <Sidebar />
-      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+      <div className='max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1'>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/explore" element={<ExplorePage />} />
-          <Route path="/likes" element={<LikesPage />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+          <Route path='/signup' element={!authUser ? <SignupPage /> : <Navigate to={"/"} />} />
+          <Route path='/explore' element={authUser ? <ExplorePage /> : <Navigate to={"/login"} />} />
+          <Route path='/likes' element={authUser ? <LikesPage /> : <Navigate to={"/login"} />} />
         </Routes>
         <Toaster />
       </div>
--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,13 +1,29 @@
 import { MdLogout } from 'react-icons/md'
+import { useAuthContext } from '../context/AuthContext'
+import { toast } from 'react-hot-toast'
 // TODO: add logout functionality
 
 const Logout = () => {
+    const { authUser, setAuthUser } = useAuthContext();
+
+    const handleLogout = async () => {
+        try {
+            const res = await fetch("/api/auth/logout", { credentials: "include" });
+            const data = await res.json();
+            console.log(data);
+            setAuthUser(null);
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+
     return (
         <>
-            <img src={"https://lh3.googleusercontent.com/a/ACg8ocJMqbyVoBPQGau0Yu-tgffJ2yt0_TA9HpI_EllDQoZiFXpx-M0g=s360-c-no"} className='w-10 h-10 rounded-full border border-gray-800' />
+            <img src={authUser?.avatarUrl} className='w-10 h-10 rounded-full border border-gray-800' />
 
             <div
                 className='cursor-pointer flex items-center p-2 rounded-lg bg-glass mt-auto border border-gray-800'
+                onClick={handleLogout}
             >
                 <MdLogout size={22} />
             </div>
